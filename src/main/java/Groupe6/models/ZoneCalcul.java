8b7348013f3de67098ca2ee9bd8af17b6474d7ae
maxime jo <maximej2014@gmail.com>
2026-01-30 14:27:16 +0100

    Ajout de quelque methode + ajout du dossier VueGrille

--- src/main/java/Groupe6/models/ZoneCalcul.java
+++ src/main/java/Groupe6/models/ZoneCalcul.java
@@ -3,8 +3,64 @@ package Groupe6.models;
 import java.util.List;
 
 public class ZoneCalcul {
-    List<Cellule> listeCellules;
-    int valeurCible;
-    TypeOperation typeOperation;
+    List<Cellule> listeCellules; // Liste des cellules de la zone de calcul
+    int valeurCible; // Valeur cible de la zone de calcul
+    TypeOperation typeOperation; // Type d'opération de la zone de calcul
 
+    /**
+     * Constructeur
+     * 
+     * @param valeurCible
+     * @param typeOperation
+     */
+    public ZoneCalcul(int valeurCible, TypeOperation typeOperation) {
+        this.valeurCible = valeurCible;
+        this.typeOperation = typeOperation;
+    }
+
+    /**
+     * Ajouter une cellule à la zone de calcul
+     * 
+     * @param cellule
+     * @return void
+     */
+    public void ajouterCellule(Cellule cellule) {
+        listeCellules.add(cellule);
+    }
+
+    /**
+     * Vérifier si la zone de calcul est valide
+     * 
+     * @return boolean
+     */
+    public boolean verifierMaths() {
+        return true;
+    }
+
+    /**
+     * Obtenir la liste des cellules de la zone de calcul
+     * 
+     * @return List<Cellule>
+     */
+    public List<Cellule> getListeCellules() {
+        return listeCellules;
+    }
+
+    /**
+     * Obtenir la valeur cible de la zone de calcul
+     * 
+     * @return int
+     */
+    public int getValeurCible() {
+        return valeurCible;
+    }
+
+    /**
+     * Obtenir le type d'opération de la zone de calcul
+     * 
+     * @return TypeOperation
+     */
+    public TypeOperation getTypeOperation() {
+        return typeOperation;
+    }
 }
