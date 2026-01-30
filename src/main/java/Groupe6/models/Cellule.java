887a26c3a685b3fe5c3e7358846ffda766b97f50
maxime jo <maximej2014@gmail.com>
2026-01-30 14:27:16 +0100

    Ajout de quelque methode + ajout du dossier VueGrille

--- src/main/java/Groupe6/models/Cellule.java
+++ src/main/java/Groupe6/models/Cellule.java
@@ -1,13 +1,32 @@
 package Groupe6.models;
 
 public class Cellule {
-    int ligne;
-    int colonne;
-    int valeur;
-    boolean estModifiable;
-    boolean estSelectionnee;
-    boolean estValide;
-    boolean estErreur;
-    ZoneCalcul zoneCalcul;
+    int ligne; // ligne de la cellule
+    int colonne; // colonne de la cellule
+    int valeur; // valeur de la cellule
+    boolean estModifiable; // si on est en mode candidat
+    boolean estSelectionnee; // si la cellule est selectionnee
+    boolean estValide; // si la cellule est valide
+    boolean estErreur; // si la cellule est en erreur
+    ZoneCalcul zoneCalcul; // zone de calcul de la cellule
 
+    /**
+     * Constructeur
+     * 
+     * @param ligne
+     * @param colonne
+     */
+    public Cellule(int ligne, int colonne) {
+        this.ligne = ligne;
+        this.colonne = colonne;
+    }
+
+    /**
+     * Vérifier si la cellule est valide
+     * 
+     * @return boolean
+     */
+    public boolean estValide() {
+        return estValide;
+    }
 }
---
+++ src/main/java/Groupe6/view/VueGrille.java
@@ -0,0 +1,7 @@
+package Groupe6.view;
+
+import Groupe6.models.Grille;
+
+public class VueGrille {
+
+}
