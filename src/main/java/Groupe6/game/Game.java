4a5da5b74a3c44a56ce11512fd1e2a756486f13c
lucasdupont8 <lucas.dupont72@gmail.com>
2026-03-13 16:19:23 +0100

    save des paramètre du joueur

--- src/main/java/Groupe6/game/Game.java
+++ src/main/java/Groupe6/game/Game.java
@@ -15,6 +15,8 @@ import Groupe6.etats.Parametres;
 import Groupe6.etats.Start;
 import Groupe6.etats.Connexion;
 import Groupe6.etats.Creation;
+import Groupe6.save.ParametresJoueur;
+import Groupe6.save.SaveManager;
 
 /**
  * Coeur du jeu : boucle update/render découplée (UPS fixe, FPS limité), délégation aux états (Start, Menu, etc.).
@@ -161,7 +163,10 @@ public class Game implements Runnable {
     public void saveGame() {
         try {
             System.out.println("Sauvegarde du jeu en cours...");
-            
+
+            ParametresJoueur pj = new ParametresJoueur(jeu.getGrille().getNomJoueur(), "Fr", 100, 100, 0);
+            SaveManager.sauvegarderParametres(pj);
+
             jeu.getGrille().saveGrille();
 
             Thread.sleep(500);
