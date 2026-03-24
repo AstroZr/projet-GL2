e2eefe6f0464bb02567fb47a901cfc6eebad223e
mimar <luciendefosse@gmail.com>
2026-03-24 12:11:36 +0100

    Modif de creation.java pour empêcher qu'un utilisateur puisse utiliser création avec un nom de profil déjà existant (précédemment cela n'écrasait même pas le profil cela se connectait juste dessus via création)

--- src/main/java/Groupe6/etats/Creation.java
+++ src/main/java/Groupe6/etats/Creation.java
@@ -188,12 +188,19 @@ public class Creation extends Etats implements MethodesEtats {
         if (bouton.isSourisEnfonce() && isIn(e, bouton)){
           String pseudo = getPseudoSaisi();
           if (pseudo != null && !pseudo.trim().isEmpty()){
-              game.setJoueurCourant(pseudo);
+            //vérif pour voir si le joueur existe déjà dans l'annuaire ou non (pour éviter de permettre création d'un profil déjà existant)
+            if(Groupe6.save.SaveManager.chargerParametres(pseudo) != null){
+                System.out.println("Erreur : Le profil '" + pseudo + "' existe déjà.");
+                textInput.clear(); 
+            }
+            else{
+                game.setJoueurCourant(pseudo);
 
-              // génération du dossier du joueur avec ses paramètres par défaut
-              Groupe6.save.ParametresJoueur pjParDefaut = new Groupe6.save.ParametresJoueur(pseudo, "Fr", 50, 50, 0);
-              Groupe6.save.SaveManager.sauvegarderParametres(pjParDefaut);
-              bouton.appliquerAction();
+                // génération du dossier du joueur avec ses paramètres par défaut
+                Groupe6.save.ParametresJoueur pjParDefaut = new Groupe6.save.ParametresJoueur(pseudo, "Fr", 50, 50, 0);
+                Groupe6.save.SaveManager.sauvegarderParametres(pjParDefaut);
+                bouton.appliquerAction();
+            }
           }
       }
       bouton.setSourisEnfonce(false);
