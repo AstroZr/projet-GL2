308e03d4e3386f29ba29c2e981f1ba4ad9c262f5
mimar <luciendefosse@gmail.com>
2026-03-26 15:21:41 +0100

    fix du fichier Creation.java, profil définie les paramètres volumes à 0 au lieu de 50 désormais

--- src/main/java/Groupe6/etats/Creation.java
+++ src/main/java/Groupe6/etats/Creation.java
@@ -206,7 +206,7 @@ public class Creation extends Etats implements MethodesEtats {
             game.setJoueurCourant(pseudo);
 
             // génération du dossier du joueur avec ses paramètres par défaut
-            Groupe6.save.ParametresJoueur pjParDefaut = new Groupe6.save.ParametresJoueur(pseudo, "Fr", 50, 50, 0);
+            Groupe6.save.ParametresJoueur pjParDefaut = new Groupe6.save.ParametresJoueur(pseudo, "Fr", 0, 0, 0);
             Groupe6.save.SaveManager.sauvegarderParametres(pjParDefaut);
             bouton.appliquerAction();
           }
